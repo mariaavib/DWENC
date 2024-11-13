27c3545d6cf8bc74b75b9b74be8e912771730b66
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2024-11-13 08:38:38 +0100

    Ejercicio rickandmorty

--- rickandmortyapi/rickandmortyapi.js
+++ rickandmortyapi/rickandmortyapi.js
@@ -1,4 +1,5 @@
-fetch('https://rickandmortyapi.com/api/character')
+function visualizar(){
+    fetch('https://rickandmortyapi.com/api/character')
     .then(response => {
         if(!response.ok){
             throw new Error("Error al cargar el fichero");
@@ -12,14 +13,14 @@ fetch('https://rickandmortyapi.com/api/character')
     .catch(error => {
         console.log(error);
     })
-
-    function mostrarPersonajes(data){
-        const contenedor = document.getElementById("contenedor");
-        for (let i = 0; i < data.results.length; i++) {
-            const personaje = data.results[i];
-            const img = document.createElement("img");
-            img.src=personaje.image;
-            img.style.width = "20%";
-            contenedor.appendChild(img);
-        }
+}
+function mostrarPersonajes(data){
+    const contenedor = document.getElementById("contenedor");
+    for (let i = 0; i < data.results.length; i++) {
+        const personaje = data.results[i];
+        const img = document.createElement("img");
+        img.src=personaje.image;
+        img.style.width = "20%";
+        contenedor.appendChild(img);
     }
+}
