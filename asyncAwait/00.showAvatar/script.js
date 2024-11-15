f4a9bc857b7dda2dec4d2b8e73cd3a150535ae8c
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2024-11-15 12:36:57 +0100

    Ejercicio asyc awai

--- asyncAwait/00.showAvatar/script.js
+++ asyncAwait/00.showAvatar/script.js
@@ -1,11 +1,12 @@
 async function showAvatar() {
     //Leer nuestro usuario de GIT
-    let response = await fetch('https://github.com/mariaavib');
-    let user = await response.json();
+    let response = await fetch('https://api.github.com/users/mariaavib');
+    let usu = await response.json();
 
     //Mostrar el avatar
     let img = document.createElement('img');
-    img.src = user.avatar_url;
-    img.className = "promise-example-avatar";
+    img.src = usu.avatar_url;
+    img.className = "avatar Maria";
     document.body.append(img);
+
 }
