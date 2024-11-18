f0c23639ad4f5c3c0d1ef56d5bc1036709e0842f
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2024-11-18 14:55:34 +0100

    Ejercicio PokeApi

--- PokeApi/app.js
+++ PokeApi/app.js
@@ -2,47 +2,45 @@
 /**
  * Ejercicio 1: Buscar y mostrar información del Pokemon con FETCH
  */
-async function informacion(){
-    let pokemonBusc = document.getElementById('pokemon-input').value;
-    console.log(pokemonBusc);
-    let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonBusc}`);
-    let pok = await response.json();
-    console.log(pok.name,pok.id,pok.sprites);
-    //console.log(pok);
 
-    const informacion = document.getElementById("pokemon-data");
-    const id = document.createElement("p");
-    id.innerText= "ID: "+pok.id;
-    informacion.appendChild(id);
-
-
-
-    for(let i=0;i<pok.sprites;i++){
-        const pokemon = pok.sprites[i];
-        const img = document.createElement("img");
-        img.src=pokemon.image;
-        informacion.appendChild(img);
-    }
-
-}
 /**
  * Función para buscar pokemon ejercicio 1 y 2.
  * 
 */
 
-async function buscarPokemon(){
-    /**
-     * Tu código aquí.
-     */
+/*async function buscarPokemon(){
+        let pokemonBusc = document.getElementById('pokemon-input').value;
+        console.log(pokemonBusc);
+        let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonBusc}`);
+        let pok = await response.json();
+        console.log(pok.name,pok.id,pok.sprites);
+        //console.log(pok);
+    
+        const informacion = document.getElementById("pokemon-data");
+        const id = document.createElement("p");
+        id.innerText= "ID: "+pok.id;
+        informacion.appendChild(id);
+        const img = document.createElement("img");
+        img.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pok.id}.png`);
+        informacion.appendChild(img);
+}*/
 
-
+function buscarPokemon(){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonBusc}`);
+    then(response => {
+        if(!response.ok){
+            throw new Error("No exixte el pokémon");
+        }else{
+            return response.json();
+        }
+    })
 
 }
-
 /**
  *  Descomentar para hacer uso de la función.
  */
-//document.getElementById('search-btn').addEventListener('click',buscarPokemon);
+document.getElementById('search-btn').addEventListener('click',buscarPokemon);
+
 
 
 /**
