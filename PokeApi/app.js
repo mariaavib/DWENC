100e91bbf0bd9ad53bef2b5d0390778b99190b4a
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2024-11-19 13:26:41 +0100

    Ejercicio PokeApi 1,2,3

--- PokeApi/app.js
+++ PokeApi/app.js
@@ -8,7 +8,7 @@
  * 
 */
 
-/*async function buscarPokemon(){
+async function buscarPokemon(){
         let pokemonBusc = document.getElementById('pokemon-input').value;
         console.log(pokemonBusc);
         let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonBusc}`);
@@ -18,46 +18,77 @@
     
         const informacion = document.getElementById("pokemon-data");
         const id = document.createElement("p");
+        const nombre = document.createElement("p");
         id.innerText= "ID: "+pok.id;
         informacion.appendChild(id);
+        nombre.innerText = "Nombre: "+pok.nombre;
+        informacion.appendChild(nombre);
         const img = document.createElement("img");
         img.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pok.id}.png`);
         informacion.appendChild(img);
-}*/
+}
 
-function buscarPokemon(){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonBusc}`);
-    then(response => {
+function buscarPokemonV2(){
+    let pokemonBusc = document.getElementById('pokemon-input').value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonBusc}`)
+    .then(response => {
         if(!response.ok){
             throw new Error("No exixte el pokémon");
-        }else{
-            return response.json();
         }
+        return response.json();
     })
-
+    .then (pokemon =>{
+        const informacion = document.getElementById("pokemon-data");
+        const id = document.createElement("p");
+        const nombre = document.createElement("p");
+        id.innerText= "ID: "+pokemon.id;
+        informacion.appendChild(id);
+        nombre.innerText = "Nombre: "+pokemon.name;
+        informacion.appendChild(nombre);
+        const img = document.createElement("img");
+        img.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`);
+        informacion.appendChild(img);
+    })
+    .catch(error =>{
+        alert(error);
+    })
 }
 /**
  *  Descomentar para hacer uso de la función.
  */
-document.getElementById('search-btn').addEventListener('click',buscarPokemon);
-
-
+/*document.getElementById('search-btn').addEventListener('click',buscarPokemonV2);*/
 
 /**
  * Ejercicio 3: buscar pokemon con JQuery AJAX.
  */
 
 function buscarPokemonJQueryAJAX(){
-    /**
-     * Tu código aquí.
-     */
+    $.ajax({
+        type: "GET",
+        url:  `https://pokeapi.co/api/v2/pokemon/${pokemonBusc}`,
+        success: function(pokemon){
+            const informacion = document.getElementById("pokemon-data");
+            const id = document.createElement("p");
+            const nombre = document.createElement("p");
+            id.innerText= "ID: "+pokemon.id;
+            informacion.appendChild(id);
+            nombre.innerText = "Nombre: "+pokemon.name;
+            informacion.appendChild(nombre);
+            const img = document.createElement("img");
+            img.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`);
+            informacion.appendChild(img);
+        },
+        error: function() {
+            alert("No exixte el pokémon");
+        }
+    })
 }
 
 /**
  * Haciendo uso de JQuery, descomentar para usar la función buscarPokemonJQueryAJAX
 */
-/**
+
 $(document).ready(function(){
     $('#search-btn').on('click', buscarPokemonJQueryAJAX);
 }); 
-*/
+
