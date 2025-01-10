9ae2361a145f36e59ba336e089d51040160dd0ca
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2025-01-10 13:32:01 +0100

    Typescript || Datos Primitivos

--- typescript/script.js
+++ typescript/script.js
@@ -1,2 +1,18 @@
-"use strict";
-console.log("Estoy trabajando en Typescript | Hola Mundo");
+var interMiami = 11;
+var fcDallas = 11;
+var celia = 1;
+var juegaCelia = true;
+function jugar(equipo1, equipo2, juegaCelia) {
+    var motivo = '';
+    if (juegaCelia) {
+        equipo1 += celia;
+        motivo = ' por que juega Celia';
+    }
+    if (equipo1 > equipo2)
+        console.log("Gana Inter Miami".concat(motivo));
+    if (equipo1 == equipo2)
+        console.log("Empatan");
+    if (equipo1 < equipo2)
+        console.log("Gana FC Dallas");
+}
+jugar(interMiami, fcDallas, juegaCelia);
--- typescript/script.ts
+++ typescript/script.ts
@@ -1,1 +1,17 @@
-console.log("Estoy trabajando en Typescript | Hola Mundo");
+let interMiami: number = 11
+let fcDallas: number = 11
+let celia: number = 1
+let juegaCelia: boolean = true
+
+function jugar(equipo1: number, equipo2: number, juegaCelia: boolean): void{
+    let motivo:string = ''
+    if(juegaCelia){
+      equipo1+= celia  
+      motivo = ' por que juega Celia'
+    } 
+    if(equipo1 > equipo2) console.log(`Gana Inter Miami${motivo}`)
+    if(equipo1 == equipo2) console.log("Empatan")
+    if(equipo1 < equipo2) console.log("Gana FC Dallas")
+}
+
+jugar(interMiami,fcDallas,juegaCelia);
